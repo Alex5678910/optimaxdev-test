42bc579392fccd2d7a4cca1322a321fd6663e76d
Alex5678910 <sgs1986@yandex.ru>
2021-06-20 18:22:14 +0400

    master Added fix to GitHub Pages

--- src/store/action-creators/cart.ts
+++ src/store/action-creators/cart.ts
@@ -5,9 +5,18 @@ export const fetchMenu = () => {
     return async (dispatch: Dispatch<MenuAction>) => {
         try {
             dispatch({type: ActionTypes.FETCH_MENU})
-            const db = await fetch('http://localhost:3001/menu', {
+            let db = await fetch('http://localhost:3001/menu', {
                 headers: { 'Content-Type': 'application/json' }
             }).then(res => res.json());
+            db = {
+                "menu": [
+                    {"title": "Cesar salad","price": 42, "quantity": 1,"id": 1},
+                    {"title": "Pizza Margherita","price": 44, "quantity": 1,"id": 2},
+                    {"title": "Pizza Neapolitan","price": 41,"quantity": 1,"id": 3},
+                    {"title": "Greece salad","price": 48,"quantity": 1,"id": 4},
+                    {"title": "Cowboy Steak","price": 42,"quantity": 1,"id": 5}
+                ]
+            };
             dispatch({type: ActionTypes.FETCH_MENU_SUCCESS, payload: db,})
         }catch (e) {
             dispatch({type: ActionTypes.FETCH_MENU_ERROR, payload: 'Произошла ошибка, проверьте сервер!'})
