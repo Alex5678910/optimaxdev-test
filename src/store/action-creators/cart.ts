6a3d8753404c8892744848a69ac47e57ed066f48
Alex5678910 <sgs1986@yandex.ru>
2021-06-20 18:45:48 +0400

    master Added fix to GitHub Pages

--- src/store/action-creators/cart.ts
+++ src/store/action-creators/cart.ts
@@ -7,7 +7,8 @@ export const fetchMenu = () => {
             dispatch({type: ActionTypes.FETCH_MENU})
             let db = await fetch('https://alex5678910.github.io/optimax-dev-test-json-api/api/db.json', {
                 headers: { 'Content-Type': 'application/json' }
-            }).then(res => res.json());
+            }).then(res => res.json())
+            ;
             dispatch({type: ActionTypes.FETCH_MENU_SUCCESS, payload: db,})
         }catch (e) {
             dispatch({type: ActionTypes.FETCH_MENU_ERROR, payload: 'Произошла ошибка, проверьте сервер!'})
